c120b8f75967d080ae34a94bab8141eab7f005f9
Jhani <jhaninaconteron03@gmail.com>
2026-03-22 16:57:04 -0500

    reset: main vacío para PR

--- backend/src/prisma.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
-
-@Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    // Prisma 7 handles this automatically or has removed $connect
-  }
-}
